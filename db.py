09480f96e9445acd90554a26bbc2796d4c7dad02
Ariel_Izralevitch <your.email@example.com>
2026-01-15 14:54:55 +0200

    add picture

--- db.py
+++ db.py
@@ -1,16 +1,69 @@
-import pandas as pd
-from models import Terrorist
-
-
-def choosing_5_terrorists(file):
-    df = pd.read_csv(file)
-    df.sort_values(by="danger_rate", ascending=False)
-    df_danger = df.head()
-    df_danger = df_danger[["name", "location","danger_rate"]]
-    return df_danger
+import os
+from pymongo import MongoClient
 
 
 
+def get_collection():
+    client = MongoClient(
+    host=os.getenv("MONGO_HOST"),
+    port=int(os.getenv("MONGO_PORT")),
+    username=os.getenv("MONGO_USERNAME"),
+    password=os.getenv("MONGO_PASSWORD"),
+    authSource=os.getenv("MONGO_AUTH_SOURCE")
+    )
+    db = client[os.getenv("MONGO_DB")]
+    return db["top_threats"]
+
+
+def insert_to_mongo(result):
+    connecting_to_mongo = get_collection()
+    connecting_to_mongo.insert_one(result)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class MongoDB:
+#     def __init__(self):
+#         self.host = os.getenv("MONGO_HOST")
+#         self.db_name = os.getenv("MONGO_DB")
+#
+#         self.client = MongoClient(f"mongodb://{self.host}:27017")
+#         self.db = self.client[self.db_name]
+#         self.users = self.db.users
+#
+#     def insert_user(self, user: dict):
+#         result = self.users.insert_one(user)
+#         return {"inserted_id": str(result.inserted_id)}
+#
+#     def get_users(self):
+#         users = list(self.users.find({}, {"_id": 0}))
+#         return users
--- mian.py
+++ mian.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, HTTPException,UploadFile
-from db import choosing_5_terrorists
+from fastapi import FastAPI, HTTPException,UploadFile,File
 import uvicorn
 import pandas as pd
 import csv
 import io
+import python_multipart
+from models import Terrorist
+from db import insert_to_mongo
 
 
 
@@ -12,14 +14,42 @@ app = FastAPI()
 
 @app.post("/top-threats")
 def top_threats(file:UploadFile):
-    if file.content_type != "text/csv":
-        return {"error": "File must be a CSV"}
-    content = file.file.read().decode("utf-8")
-    reader = csv.reader(io.StringIO(content))
-    return choosing_5_terrorists(reader)
-
-
-
+    df = pd.read_csv(file.file)
+    df = df.sort_values(by="rate_danger", ascending=False).head(5)
+    result = []
+    for _, row in df.iterrows():
+        threat = Terrorist(
+        name=row["name"],
+        location=row["location"],
+        rate_danger=int(row["rate_danger"])
+        )
+        result.append(threat)
+    insert_to_mongo(result)
+    return {
+    "count": len(result),
+    "top": result
+    }
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
